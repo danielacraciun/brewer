2b234a7b44ecf852c0bda3bb4895f55fb8199c4f
Daniela Craciun <craciundaniela19@gmail.com>
2016-03-27 18:14:19 +0300

    Staging on heroku

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -6,7 +7,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'to-be-generated'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
